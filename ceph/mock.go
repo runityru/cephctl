8fb3a4d7f86f18a056ef12e58355ceb4a3027c41
Igor Shishkin <me@teran.dev>
2024-04-28 13:52:45 +0300

    Sort ceph driver methods order
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- ceph/mock.go
+++ ceph/mock.go
@@ -17,14 +17,14 @@ func NewMock() *Mock {
 	return &Mock{}
 }
 
-func (m *Mock) DumpConfig(_ context.Context) (models.CephConfig, error) {
-	args := m.Called()
-	return args.Get(0).(models.CephConfig), args.Error(1)
-}
-
 func (m *Mock) ApplyCephConfigOption(ctx context.Context, section, key, value string) error {
 	args := m.Called(section, key, value)
 	return args.Error(0)
+}
+
+func (m *Mock) DumpConfig(_ context.Context) (models.CephConfig, error) {
+	args := m.Called()
+	return args.Get(0).(models.CephConfig), args.Error(1)
 }
 
 func (m *Mock) RemoveCephConfigOption(ctx context.Context, section, key string) error {
