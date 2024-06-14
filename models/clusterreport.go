af0b2ee04b0a8ec5f053c670ff2732af78c1801c
Igor Shishkin <me@teran.dev>
2024-06-14 20:05:39 +0300

    Just reorder ClusterReport structure fields alphabetically for easier usage (#39)

--- models/clusterreport.go
+++ models/clusterreport.go
@@ -1,26 +1,26 @@
 package models
 
 type ClusterReport struct {
-	HealthStatus                 ClusterStatusHealth
+	AllowCrimson                 bool
 	Checks                       []ClusterStatusCheck
+	Devices                      []Device
+	HealthStatus                 ClusterStatusHealth
 	MutedChecks                  []ClusterStatusMutedCheck
 	NumMons                      uint8
 	NumMonsInQuorum              uint8
-	AllowCrimson                 bool
-	StretchMode                  bool
 	NumOSDs                      uint16
-	NumOSDsWithoutClusterAddress uint16
-	NumOSDsIn                    uint16
-	NumOSDsUp                    uint16
+	NumOSDsByDeviceType          map[string]uint16
 	NumOSDsByRelease             map[string]uint16
 	NumOSDsByVersion             map[string]uint16
-	NumOSDsByDeviceType          map[string]uint16
-	Devices                      []Device
+	NumOSDsIn                    uint16
+	NumOSDsUp                    uint16
+	NumOSDsWithoutClusterAddress uint16
+	NumPGs                       uint32
+	NumPGsByState                map[string]uint32
+	NumPools                     uint16
+	StretchMode                  bool
 	TotalOSDCapacityKB           uint64
 	TotalOSDUsedDataKB           uint64
 	TotalOSDUsedMetaKB           uint64
 	TotalOSDUsedOMAPKB           uint64
-	NumPools                     uint16
-	NumPGs                       uint32
-	NumPGsByState                map[string]uint32
 }
