1516ac1290e2392a67f0421991a9b18bc873b463
Igor Shishkin <me@teran.dev>
2024-06-15 09:34:40 +0300

    Fix unquoted characters passed to the command (#42)
    Signed-off-by: Igor Shishkin <me@teran.dev>

--- ceph/cmd.go
+++ ceph/cmd.go
@@ -8,9 +8,22 @@ import (
 
 func mkCommand(cephBinary string, args []string) (string, []string) {
 	outCmd := shellCommand
-	outArgs := []string{"-c", strings.Join(append([]string{cephBinary}, args...), " ")}
+
+	escapedArgs := []string{}
+	for _, arg := range args {
+		escapedArgs = append(escapedArgs, handleArg(arg))
+	}
+
+	outArgs := []string{"-c", strings.Join(append([]string{cephBinary}, escapedArgs...), " ")}
 
 	log.Tracef("preparing command: `%s` `%#v`", outCmd, outArgs)
 
 	return shellCommand, outArgs
+}
+
+func handleArg(arg string) string {
+	arg = strings.ReplaceAll(arg, `\`, `\\`)
+	arg = strings.ReplaceAll(arg, "'", `\'`)
+
+	return "'" + arg + "'"
 }
--- ceph/cmd_test.go
+++ ceph/cmd_test.go
@@ -12,6 +12,46 @@ func TestMkCommand(t *testing.T) {
 	bin, args := mkCommand("testcmd", []string{"arg1", "arg2"})
 	r.Equal(shellCommand, bin)
 	r.Equal([]string{
-		shellArg, "testcmd arg1 arg2",
+		shellArg, "testcmd 'arg1' 'arg2'",
 	}, args)
+}
+
+func TestHandleArg(t *testing.T) {
+	type testCase struct {
+		name   string
+		in     string
+		expOut string
+	}
+
+	tcs := []testCase{
+		{
+			name:   "simple string",
+			in:     "simple string",
+			expOut: "'simple string'",
+		},
+		{
+			name:   "string with special characters",
+			in:     `!@#$%^&*()_-+=\/:'"`,
+			expOut: `'!@#$%^&*()_-+=\\/:\'"'`,
+		},
+		{
+			name:   "single-quoted string",
+			in:     `'quoted string'`,
+			expOut: `'\'quoted string\''`,
+		},
+		{
+			name:   "double-quoted string",
+			in:     `"quoted string"`,
+			expOut: `'"quoted string"'`,
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			r := require.New(t)
+
+			v := handleArg(tc.in)
+			r.Equal(tc.expOut, v)
+		})
+	}
 }
